7f1ab3e37b045309ffb25d0f30be277f323f893a
zqasem <ziyaad.qasem@materna.group>
2025-05-08 15:22:12 +0200

    fixing hosts label

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,11 +1,28 @@
-terraform {
-  required_version = ">= 1.0"
+variable "remote_ips" {
+  type = list(string)
 }
 
-provider "null" {}
+variable "ssh_user" {
+  type = string
+}
 
-resource "null_resource" "example" {
-  provisioner "local-exec" {
-    command = "echo Hello from Terraform"
+variable "ssh_private_key" {
+  type = string
+}
+
+resource "null_resource" "remote_exec" {
+  count = length(var.remote_ips)
+
+  connection {
+    type        = "ssh"
+    host        = var.remote_ips[count.index]
+    user        = var.ssh_user
+    private_key = replace(var.ssh_private_key, "\\n", "\n")
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "echo Hello from Terraform on ${var.remote_ips[count.index]} > /tmp/hello-from-terraform.txt"
+    ]
   }
 }
